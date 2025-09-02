e428526085c336f8e0bac0d79c0937b3728417cf
jliu1453 <jliu1453@outlook.com>
2025-09-01 21:03:49 -0700

    Update repo.java

--- northerners/body/src/main/java/com/northerners/mapper/repo.java
+++ northerners/body/src/main/java/com/northerners/mapper/repo.java
@@ -1,3 +1,4 @@
+//generic inteface for all mappers
 package com.northerners.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,9 +7,10 @@ import java.util.List;
 
 
 public interface repo<T, ID> {
-    List<T> findAll();
+    List<T> findAll(); //return list of entity
     T findById(ID id);
     int insert(T entity);
     int update(T entity);
     int delete(ID id);
 }
+
